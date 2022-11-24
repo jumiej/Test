07e406b330851ff6e2cf2f7c97cf9d7864f8c7d5
jumie <akinyodejummy177@gmail.com>
2022-11-24 11:49:52 +0000

    test this git

---
+++ java.js
@@ -0,0 +1,45 @@
+// const newParagraph = document.createElement('p');
+// newParagraph.innerText = "This is a dynamically created paragraph"
+// document.body.appendChild(newParagraph)
+
+
+// function start () {
+//     for(var i = 0; i < 5; i++ )
+//     console.log(i)
+// }
+
+// start();
+
+
+
+// function greet(name){
+//     console.log("hello" + ' ' + name);
+//     greet("jumie")
+// }
+
+
+// 
+
+// // let myImage = bulbon.git
+
+// const lightOn = document.getElementById('myImage').scr='bulbon.gif';
+
+// let x, y, z;
+// x = 5;
+// y = 6;
+// z = x + y;
+// let addtion = document.getElementById('addition').innerHTML = "The value of z is "
+
+//  let greet = document.getElementById("addition").innerHTML = "hello dolly";
+
+//  let a, b, c;
+//  a = 5;
+//  b = 6;
+//  c = a + b;
+
+
+
+// document.getElementById("demo").innerHTML = "i am a gir/l";
+
+let lastName;
+lastName = "Doe"
